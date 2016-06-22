47cd68be5489ce3cf450419829e3f851cf510e71
Leko <leko.noor@gmail.com>
2016-06-22 11:12:34 +0900

    test: Add schema structure test

---
+++ test/swagger.spec.js
@@ -0,0 +1,16 @@
+
+const fs = require('fs')
+const assert = require('assert')
+const tv4 = require('tv4')
+const load = require('js-yaml').safeLoad
+const refParser = require('json-schema-ref-parser')
+const official = require('swagger-schema-official/schema')
+
+describe('swagger', () => {
+  it('must compatible with OpenAPI', () => {
+    const yml = fs.readFileSync(`${__dirname}/../swagger.yml`)
+    return refParser.dereference(load(yml))
+      .then(schema => tv4.validate(schema, official))
+      .then(valid => assert.ok(valid))
+  })
+})
